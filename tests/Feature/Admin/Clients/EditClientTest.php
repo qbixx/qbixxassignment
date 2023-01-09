0612c3dabe4c49cc76fab1d8ac6c96652fe2b3e2
Mladen Mitic <mladen.mitic.dev@gmail.com>
2023-01-09 17:41:45 +0100

    Add tests for edit and update

---
+++ tests/Feature/Admin/Clients/EditClientTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Admin\Clients;
+
+use App\Enums\RoutesEnum;
+use Domain\Clients\Models\Client;
+use Tests\FeatureTest;
+
+class EditClientTest extends FeatureTest
+{
+    /**
+     * @test
+     */
+    public function a guest can edit a client(): void
+    {
+        $client = Client::factory()->create();
+
+        $this->get(route(RoutesEnum::ADMIN_EDIT_CLIENT, $client->id))
+            ->assertOk();
+    }
+}
---
+++ tests/Feature/Admin/Clients/UpdateClientTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Admin\Clients;
+
+use App\Enums\RoutesEnum;
+use Domain\Clients\Models\Client;
+use Tests\FeatureTest;
+
+class UpdateClientTest extends FeatureTest
+{
+    private Client $client;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->client = Client::factory()->create();
+    }
+
+    /**
+     * @test
+     */
+    public function a guest can update clients_name(): void
+    {
+        $formData = [
+            'name' => 'New name',
+        ];
+
+        $this->patch(
+            route(
+                RoutesEnum::ADMIN_UPDATE_CLIENT,
+                [
+                    'client' => $this->client->id
+                ]
+            ),
+            $formData
+        )->assertRedirect(route(RoutesEnum::ADMIN_INDEX_CLIENTS));
+
+        $client = Client::findOrFail($this->client->id);
+
+        $this->assertSame('New name', $client->name);
+    }
+
+    /**
+     * @test
+     * 
+     * @dataProvider invalidData
+     */
+    public function a guest can not_update clients_name_with_invalid_data(string $columnName, string $value): void
+    {
+        $formData = [
+            $columnName => $value,
+        ];
+
+        $this->patch(
+            route(
+                RoutesEnum::ADMIN_UPDATE_CLIENT,
+                [
+                    'client' => $this->client->id
+                ]
+            ),
+            $formData
+        )->assertSessionHasErrors($columnName);
+    }
+
+    public function invalidData(): array
+    {
+        return [
+            'required' => ['name', ''],
+            'min 4' => ['name', 'fre'],
+            'max 30' => ['name', 'Do you know someone with a name longer than 30 characters?'],
+        ];
+    }
+}
