be6af86020cc410d2ee6e06bb8d92c0eed100b8b
Mladen Mitic <mladen.mitic.dev@gmail.com>
2023-01-10 17:54:21 +0100

    Add migration for items

---
+++ database/migrations/2023_01_10_164243_create_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->string('paragraph', 300);
+            $table->string('type', 30);
+            $table->foreignId('client_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+};
