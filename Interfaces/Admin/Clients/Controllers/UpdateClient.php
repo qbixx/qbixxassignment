e630cd737905b8ea220171dc0feabb0b840081f8
Mladen Mitic <mladen.mitic.dev@gmail.com>
2023-01-12 17:52:50 +0100

    Separate requests for store and update

--- Interfaces/Admin/Clients/Controllers/UpdateClient.php
+++ Interfaces/Admin/Clients/Controllers/UpdateClient.php
@@ -7,11 +7,11 @@ namespace Interfaces\Admin\Clients\Controllers;
 use App\Enums\RoutesEnum;
 use App\Http\Controllers\Controller;
 use Domain\Clients\Models\Client;
-use Interfaces\Admin\Clients\Requests\StoreClientRequest;
+use Interfaces\Admin\Clients\Requests\UpdateClientRequest;
 
 class UpdateClient extends Controller
 {
-    public function __invoke($id, StoreClientRequest $request)
+    public function __invoke($id, UpdateClientRequest $request)
     {
         $client = Client::findOrFail($id);
 
--- Interfaces/Admin/Clients/Requests/UpdateClientRequest.php
+++ Interfaces/Admin/Clients/Requests/UpdateClientRequest.php
@@ -6,12 +6,15 @@ namespace Interfaces\Admin\Clients\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreClientRequest extends FormRequest
+class UpdateClientRequest extends FormRequest
 {
     public function rules(): array
     {
         return [
             'name' => ['required', 'string', 'min:4', 'max:30'],
+            'title' => ['required', 'array'],
+            'paragraph' => ['required', 'array'],
+            'type' => ['required', 'array'],
         ];
     }
 }
