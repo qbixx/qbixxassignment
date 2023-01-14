ecc9e726058f6d267a760dbb6ffe9b700950401c
Mladen Mitic <mladen.mitic.dev@gmail.com>
2023-01-14 14:50:59 +0100

    Generate URLs on the server

--- Interfaces/Admin/Clients/Controllers/IndexClients.php
+++ Interfaces/Admin/Clients/Controllers/IndexClients.php
@@ -4,16 +4,24 @@ declare(strict_types=1);
 
 namespace Interfaces\Admin\Clients\Controllers;
 
+use App\Enums\RoutesEnum;
 use App\Http\Controllers\Controller;
 use Domain\Clients\Models\Client;
 use Inertia\Inertia;
 use Inertia\Response;
+use URL;
 
 class IndexClients extends Controller
 {
     public function __invoke(): Response
     {
-        $clients = Client::all();
+        $clients = Client::all()
+            ->map(function ($user) {
+                $user->editUrl = URL::route(RoutesEnum::ADMIN_EDIT_CLIENT, $user);
+                $user->deleteUrl = URL::route(RoutesEnum::ADMIN_DELETE_CLIENT, $user);
+
+                return $user;
+            });
 
         return Inertia::render(
             'Admin/Clients/Index',
