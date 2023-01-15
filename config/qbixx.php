f0898008494547845665bc4f94844fd2947c7db6
Mladen Mitic <mladen.mitic.dev@gmail.com>
2023-01-15 13:53:16 +0100

    Add default items file

--- config/qbixx.php
+++ config/qbixx.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 return [
     'default_items' => [
         'storage' => env('DEFAULT_ITEMS_STORAGE', 'local'),
-        'path' => env('DEFAULT_ITEMS_PATH', 'default-items.json'),
+        'path' => env('DEFAULT_ITEMS_PATH', 'default-client-items.json'),
     ],
 ];
