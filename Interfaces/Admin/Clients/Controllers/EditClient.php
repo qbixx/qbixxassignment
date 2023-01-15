56f2b4dfc5e43436ab3628700d239e5729b11a3a
Mladen Mitic <mladen.mitic.dev@gmail.com>
2023-01-15 18:54:24 +0100

    Refactor

--- Interfaces/Admin/Clients/Controllers/EditClient.php
+++ Interfaces/Admin/Clients/Controllers/EditClient.php
@@ -7,10 +7,11 @@ namespace Interfaces\Admin\Clients\Controllers;
 use App\Http\Controllers\Controller;
 use Domain\Clients\Enums\ItemType;
 use Domain\Clients\Models\Client;
+use Illuminate\View\View;
 
 class EditClient extends Controller
 {
-    public function __invoke(int $id)
+    public function __invoke(int $id): View
     {
         $client = Client::with('items')->findOrFail($id);
         $itemTypes = ItemType::cases();
