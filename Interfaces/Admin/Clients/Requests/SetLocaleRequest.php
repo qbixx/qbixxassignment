27206a02f4eed9f4b1f98dd2a37108055ce65313
Mladen Mitic <mladen.mitic.dev@gmail.com>
2023-01-15 19:07:39 +0100

    Refactor

--- Interfaces/Admin/Clients/Requests/SetLocaleRequest.php
+++ Interfaces/Admin/Clients/Requests/SetLocaleRequest.php
@@ -4,14 +4,16 @@ declare(strict_types=1);
 
 namespace Interfaces\Admin\Clients\Requests;
 
+use App\Enums\AppLanguage;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
 
 class SetLocaleRequest extends FormRequest
 {
     public function rules(): array
     {
         return [
-            'language' => ['required', 'string', 'in:en,nl,fr'],
+            'language' => ['required', 'string', new Enum(AppLanguage::class)],
         ];
     }
 }
